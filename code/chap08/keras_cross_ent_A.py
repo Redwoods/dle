0c86a7252aafcf9728f33141cf916e767c88bb25
Sang Hoon Yi <chaos21c@gmail.com>
2023-05-21 10:50:08 +0900

    Add files via upload

--- code/chap08/keras_cross_ent_A.py
+++ code/chap08/keras_cross_ent_A.py
@@ -7,12 +7,17 @@ y_pred = [[0.8], [0.3], [0.5], [0.9]]
 # y_pred = [[1.0], [0.3], [0.5], [1.0]]   
 bce = tf.keras.losses.BinaryCrossentropy()
 print(bce(y_true, y_pred).numpy())
+bce2 = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+print(bce2(y_true, y_pred).numpy())
+
       
 # CategoricalCrossentropy
 y_true = [[0.0, 1.0, 0.0], [0.0, 0.0, 1.0], [1.0, 0.0, 0.0]] # 고양이, 호랑이, 강아지
 y_pred = [[0.6, 0.3, 0.1], [0.3, 0.6, 0.1], [0.1, 0.7, 0.2]]
 cce = tf.keras.losses.CategoricalCrossentropy ()
-print(cce(y_true, y_pred).numpy ())      
+print(cce(y_true, y_pred).numpy ())  
+cce2 = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
+print(cce2(y_true, y_pred).numpy ())    
 
 y_true = [ 12 , 20 , 29 , 60 ]
 y_pred = [ 14 , 18 , 27 , 55 ]
